0db9007f22b25e746e1811270311daf5d0d7d631
JaiRaga <raga.j.santhosh@gmail.com>
2022-05-05 21:02:30 +0530

    :zap: created the background

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import bg from "./assets/bg.jpeg";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Universe.</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        source={bg}
+        style={styles.background}
+        resizeMode="contain"
+      >
+        <View style={styles.circle}>
+          <View style={styles.innerCircle} />
+        </View>
+      </ImageBackground>
+
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -16,5 +26,27 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#242D34",
+  },
+  background: {
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 15,
+  },
+  circle: {
+    width: 75,
+    height: 75,
+    borderRadius: 50,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  innerCircle: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    backgroundColor: "#242D34",
   },
 });
